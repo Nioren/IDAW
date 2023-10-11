ef497d72c9ecfecbe7f7fd11c2606420d2f40171
Nioren <thomas.chauvel@telecom-sudparis.eu>
2023-10-11 11:44:12 +0200

    tp3

---
+++ TP3/connected.php
@@ -0,0 +1,24 @@
+<?php
+    $users = array(
+        'riri' => 'fifi',
+        'yoda' => 'maitrejedi',
+        'thomas' => 'chauvel');
+    $login = "anonymous";
+    $errorText = "";
+    $successfullyLogged = false;
+    if(isset($_POST['login']) && isset($_POST['password'])) {
+        $tryLogin=$_POST['login'];
+        $tryPwd=$_POST['password'];
+        if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
+            $successfullyLogged = true;
+            $login = $tryLogin;
+        } else
+            $errorText = "Erreur de login/password";
+    } else
+        $errorText = "Merci d'utiliser le formulaire de login";
+    if(!$successfullyLogged) {
+        echo $errorText;
+    } else {
+        echo "<h1>Bienvenu ".$login."</h1>";
+    }
+?>
