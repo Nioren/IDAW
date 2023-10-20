ab11129d053b59bc195ad8cd23981269860352f2
Nioren <thomas.chauvel@telecom-sudparis.eu>
2023-10-20 10:41:24 +0200

    tp5exo et maj tp4

--- TP4/exo5/init_pdo.php
+++ TP4/exo5/init_pdo.php
@@ -1,10 +1,10 @@
 <?php
-    define($dbHost,'127.0.0.1');
-    define($dbName,'dbtest');
-    define($dbUser,'root');
-    define($dbPassword,'');
+    define('DB_HOST','127.0.0.1');
+    define('DB_NAME','dbtest');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
 try {
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+    $pdo = new PDO("mysql:host=" . DB_HOST .";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
